c2c617fae18205f9e5c7e0dddea6e0c3cd53c17a
jeancmr <jean.madiedo08@gmail.com>
2026-01-10 14:22:34 -0500

    chore: initialize shadcn/ui

--- src/UserManagementApp.tsx
+++ src/UserManagementApp.tsx
@@ -1,3 +1,12 @@
+import { Button } from './components/ui/button';
+
 export const UserManagementApp = () => {
-  return <div>UserManagementApp</div>;
+  return (
+    <div className="h-full bg-indigo-950">
+      <h1 className="text-2xl text-white mb-12">User Management</h1>
+      <Button variant="destructive" className="text-2xl">
+        Click
+      </Button>
+    </div>
+  );
 };
